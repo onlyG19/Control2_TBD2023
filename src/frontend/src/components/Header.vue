c08eb2906b610e86411303a41762abe36bc2d3bf
José Llancamil <jose.llancamil@usach.cl>
2023-10-08 22:25:11 -0300

    [José Llancamil] - Separación en componentes
    Mini fix de front para separar elementos en componentes.

---
+++ src/frontend/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- Encabezado del sitio -->
+    <header>
+      <!-- Cambio de fuente para "My To-Do" -->
+      <h1 class="title" style="font-family: 'NuevaFuente', sans-serif;">My To-Do</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para el componente Header */
+  header {
+    color: #fbfff1;
+    background-color: #3066be;
+    font-size: 1.6rem;
+    padding: 2rem;
+    margin: 0;
+  }
+  
+  header h1 {
+    text-align: center;
+    background-color: #3066be;
+    margin: 0;
+  }
+  </style>
